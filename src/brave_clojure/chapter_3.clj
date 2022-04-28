b21068bd0a89802099609cc33e1fdf73b1b67ce3
sakib <nsakib.cse@gmail.com>
2022-04-29 03:01:53 +0600

    Use vector

--- src/brave_clojure/chapter_3.clj
+++ src/brave_clojure/chapter_3.clj
@@ -1,10 +1,14 @@
 (ns brave-clojure.chapter-3)
 
 (defn exercise-1
+  "Use the str, vector, list, hash-map, and hash-set functions."
   [invader native]
-  (str invader " invades " native))
+  (str invader " invades "
+       (apply str
+         (interpose ", " (drop-last native)))
+         " and "
+         (last native)))
 
 
 (comment
-  (exercise-1 "Putin" "Ukrains")
-  )
+  (exercise-1 "Putin" (vector "Ukrain" "Georgia" "Syria")))
